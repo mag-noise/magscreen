23d822ae6207ccc4d12f0948f58b0e3f0da64a65
Chris Piker <cpiker@users.noreply.github.com>
2022-07-05 13:38:03 -0500

    Correct inaccurate comments

--- test/rotation_test.py
+++ test/rotation_test.py
@@ -4,7 +4,7 @@ import math
 
 """Testing rotations"""
 
-def screenRotate(lPts, degree):
+def rotate2D(lPts, degree):
 
 	import math as M
 
@@ -13,9 +13,8 @@ def screenRotate(lPts, degree):
 	2. Rotate the points about the centroid
 	3. Return a list of rotated points.
 
-	This is a screen coordinates oriented function, so positive Y is typically
-	oriented down the page and positive angles are assumed to run clockwise
-	for example:
+	For screen coordinates, Y is typically oriented down the page thus positive
+	angles are assumed to run clockwise for example:
 
    +----------------> +x
    |\) positive angle
@@ -58,18 +57,16 @@ def screenRotate(lPts, degree):
 	lOut = [ [pt[0] - centroid[0], pt[1] - centroid[1]] for pt in lPts]
 
 	# Calcutate rotation matrix.  Trig functions are expensive do them once
-
 	rad = (degree * M.pi) / 180
-
 	R = ( ( M.cos(rad), - M.sin(rad) ), (M.sin(rad), M.cos(rad) ) )
 
-	# Now rotate
+	# Rotate
 	lOut = [ 
 		[pt[0]*R[0][0] - pt[1]*R[0][1], pt[0]*R[1][0] + pt[1]*R[1][1] ]  
 		for pt in lOut 
 	]
 
-	# Now translate by negative the centroid
+	# Translate back to the centroid
 	lOut = [ [pt[0] + centroid[0], pt[1] + centroid[1] ] for pt in lOut ]
 
 	return lOut
@@ -88,38 +85,38 @@ class TestRotation(unittest.TestCase):
 			self.assertTrue(abs(lRot[i][0] - lExpect[i][0]) < epsilon)
 			self.assertTrue(abs(lRot[i][1] - lExpect[i][1]) < epsilon)
 
-	def test_rotation_45ccw(self):
+	def test_rotation_45cw(self):
 		"""
-		Test rotation 45 degrees counter clockwise
+		Test rotation 45 degrees clockwise (i.e. pitch down)
 		"""
 		a = 1/math.sqrt(2)
 
 		lOrig   = [(-1, 0), (1,0)]
 		lExpect = [(-a,-a), (a,a)]
-		lRot = screenRotate(lOrig, 45)
+		lRot = rotate2D(lOrig, 45)
 		
 		self.check(lRot, lExpect)
 
-	def test_rotation_45cw(self):
+	def test_rotation_45ccw(self):
 		"""
-		Test rotation 45 degrees clockwise
+		Test rotation 45 degrees counter clockwise (i.e. pitch up)
 		"""
 		a = 1/math.sqrt(2)
 
 		lOrig   = [(-1,0), (1, 0)]
 		lExpect = [(-a,a), (a,-a)]
 
-		lRot = screenRotate(lOrig, -45)
+		lRot = rotate2D(lOrig, -45)
 		
 		self.check(lRot, lExpect)
 		
-	def test_rotation_45ccw_oc(self):
-		"""Test rotation 45 count-clockwise, off center"""
+	def test_rotation_45cw_oc(self):
+		"""Test rotation 45 clockwise, off center"""
 		a = 1/math.sqrt(2)
 
 		lOrig =    [(-1+1, 0+1), (1+1,0+1) ]
 		lExpect =  [(-a+1,-a+1), (a+1,a+1) ]
-		lRot  = screenRotate(lOrig, 45)
+		lRot  = rotate2D(lOrig, 45)
 
 		self.check(lRot, lExpect)
 
